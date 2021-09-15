2ea5739d03ce640275b25e040c6a48f96e277c25
Jacob <jamsbury95@hotmail.com>
2021-09-14 21:08:16 -0700

    converted code to functions

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
 'use strict'
 
-alert('Hello adventurer, Welcome to our escape rooms!');
+let answer = '';
+let color = '';
 
-let answer = prompt('Which adventure would you prefer? Desert, Magic, or Wonderland?');
+function greetUser() {
+    alert('Hello adventurer, Welcome to our escape rooms!');
+}
 
- let color = '';
+function promptUser() {
+    answer = prompt('Which adventure would you prefer? Desert, Magic, or Wonderland?');
+}
 
-if(answer.toLowerCase() === 'desert'){
-    alert('Get ready to take on the Pharoh in our pyramid themed escape room!');
-    color = 'yellow';
-}
-else if(answer.toLowerCase() === 'magic'){
-    alert('Come join us at a school for witchcraft and wizardry, and enjoy a fun escape room!');
-    color = 'purple';
-}
-else if(answer.toLowerCase() === 'wonderland'){
-    alert('Join us on an adventure through a twist of a wonderland, in an exciting and challenging escape room!');
-    color = 'green';
-}
-else{
-    alert('Check out our fun and enjoyable adventures!');
-    color = 'blue';
+function selectColor() {
+    let color = '';
+    if(answer.toLowerCase() === 'desert'){
+        alert('Get ready to take on the Pharoh in our pyramid themed escape room!');
+        color = 'yellow';
+    }
+    else if(answer.toLowerCase() === 'magic'){
+        alert('Come join us at a school for witchcraft and wizardry, and enjoy a fun escape room!');
+        color = 'purple';
+    }
+    else if(answer.toLowerCase() === 'wonderland'){
+        alert('Join us on an adventure through a twist of a wonderland, in an exciting and challenging escape room!');
+        color = 'green';
+    }
+    else{
+        alert('Check out our fun and enjoyable adventures!');
+        color = 'blue';
+    };
+    document.body.style.color = color;
 };
 
-document.body.style.color = color;
+
+// alert('Hello adventurer, Welcome to our escape rooms!');
+// let answer = prompt('Which adventure would you prefer? Desert, Magic, or Wonderland?');
+// let color = '';
+// if(answer.toLowerCase() === 'desert'){
+//     alert('Get ready to take on the Pharoh in our pyramid themed escape room!');
+//     color = 'yellow';
+// }
+// else if(answer.toLowerCase() === 'magic'){
+//     alert('Come join us at a school for witchcraft and wizardry, and enjoy a fun escape room!');
+//     color = 'purple';
+// }
+// else if(answer.toLowerCase() === 'wonderland'){
+//     alert('Join us on an adventure through a twist of a wonderland, in an exciting and challenging escape room!');
+//     color = 'green';
+// }
+// else{
+//     alert('Check out our fun and enjoyable adventures!');
+//     color = 'blue';
+// };
+
+// document.body.style.color = color;
