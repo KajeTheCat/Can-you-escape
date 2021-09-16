6d6629bf2e21a709c16bd9883e314fdcb6db9a79
Jacob <jamsbury95@hotmail.com>
2021-09-15 22:24:08 -0700

    tried figuring out 'for' to post multiple stars dependant on the response.

--- app.js
+++ app.js
@@ -32,25 +32,8 @@ function selectColor() {
     document.body.style.color = color;
 };
 
-
-// alert('Hello adventurer, Welcome to our escape rooms!');
-// let answer = prompt('Which adventure would you prefer? Desert, Magic, or Wonderland?');
-// let color = '';
-// if(answer.toLowerCase() === 'desert'){
-//     alert('Get ready to take on the Pharoh in our pyramid themed escape room!');
-//     color = 'yellow';
-// }
-// else if(answer.toLowerCase() === 'magic'){
-//     alert('Come join us at a school for witchcraft and wizardry, and enjoy a fun escape room!');
-//     color = 'purple';
-// }
-// else if(answer.toLowerCase() === 'wonderland'){
-//     alert('Join us on an adventure through a twist of a wonderland, in an exciting and challenging escape room!');
-//     color = 'green';
-// }
-// else{
-//     alert('Check out our fun and enjoyable adventures!');
-//     color = 'blue';
-// };
-
-// document.body.style.color = color;
+function Rate(){
+    prompt('how many stars would you give us?');
+    for(stars = 0;  )
+    document.write('<img src= "https://www.seekpng.com/ima/u2q8a9w7o0t4t4y3/">')
+};
