f5dbd97fe1880b97ff333356e4be879a0d2552ff
Jacob <jamsbury95@hotmail.com>
2021-09-16 19:13:48 -0700

    test

--- app.js
+++ app.js
@@ -13,30 +13,34 @@ function promptUser() {
 
 function selectColor() {
     let color = '';
-    if(answer.toLowerCase() === 'desert'){
+    if (answer.toLowerCase() === 'desert') {
         alert('Get ready to take on the Pharoh in our pyramid themed escape room!');
         color = 'yellow';
     }
-    else if(answer.toLowerCase() === 'magic'){
+    else if (answer.toLowerCase() === 'magic') {
         alert('Come join us at a school for witchcraft and wizardry, and enjoy a fun escape room!');
         color = 'purple';
     }
-    else if(answer.toLowerCase() === 'wonderland'){
+    else if (answer.toLowerCase() === 'wonderland') {
         alert('Join us on an adventure through a twist of a wonderland, in an exciting and challenging escape room!');
         color = 'green';
     }
-    else{
+    else {
         alert('Check out our fun and enjoyable adventures!');
         color = 'blue';
     };
     document.body.style.color = color;
 };
 
-function Rate(){
+function Rate() {
     let userResponse = parseInt(prompt('how many stars would you give us?'));
     console.log(userResponse);
-    for(let i = 0;i < userResponse;i++){
+    for (let i = 0; i < userResponse; i++) {
         console.log(i)
         document.write('<img src="https://via.placeholder.com/150">');
     }
 };
+
+
+var a = 42;
+var b = "forty-two";
